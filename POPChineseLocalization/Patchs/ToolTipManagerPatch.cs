ebbd82cc69d039aced75d478c0eec15f34903406
Gaoyao Massimo Hu <ghzero97@gmail.com>
2024-07-14 09:16:16 +0100

    Add ToolTipManagerPatch.cs

---
+++ POPChineseLocalization/Patchs/ToolTipManagerPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace POPChineseLocalization.Patch
+{
+    [HarmonyPatch(typeof(ToolTipManager), nameof(ToolTipManager.showTooltip))]
+    public class ToolTipManagerPatch
+    {
+        public static void Prefix(ToolTipManager.Position pos, ref string text, bool showInstant )
+        {
+            if (!text.TryTranslate(out var translated)) return;
+            text = translated;
+        }
+    }
+}
